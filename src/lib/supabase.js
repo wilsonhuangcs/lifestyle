2d4eebaf53a18767c070cbc18d4f757457ffda69
Wilson Huang <124078123+wilsonhuangcs@users.noreply.github.com>
2026-04-25 14:08:58 -0500

    Delete src directory

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://dswetxilqyzvrgocqobf.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzd2V0eGlscXl6dnJnb2Nxb2JmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNjEyMzEsImV4cCI6MjA4ODczNzIzMX0.Zh3z4fMfvGeH5w7qsoQZ7LpSC43KDAgt8JNVI6OHGmE';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
